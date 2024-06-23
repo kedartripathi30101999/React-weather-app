cc207c4eaca42e9c3b3f6c1a60d96aeb9485d807
kedar tripathi <tripathikd786@gmail.com>
2024-06-23 22:03:36 +0530

    deployed

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './index.css'
+import Weather from "./Components/Weather";
+const App=()=>
+    {
+        return(
+            <div className="app">
+               <Weather/>
+            </div>
+        )
+    }
+export default App
